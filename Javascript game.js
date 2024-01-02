3d8c8f22fc6d12052cf03c5e92635298616df4f0
sg6724 <151558855+sg6724@users.noreply.github.com>
2024-01-02 10:46:13 +0530

    Add files via upload

---
+++ Javascript game.js
@@ -0,0 +1,6 @@
+let gamenum=40;
+let usernum= prompt("enter the game number");
+while(usernum!=gamenum){
+   usernum= prompt("enter the correct number!!");
+}
+alert("Yay!! This is the correct number");
